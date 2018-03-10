761361c1cba9706182dc11ec467f913722792d59
chris-malloy <chrismalloymusic@gmail.com>
2018-03-10 16:31:46 -0500

    deposit and withdraw action/reducer tests passing

--- src/reducers/balance.js
+++ src/reducers/balance.js
@@ -4,7 +4,11 @@ export default (state = 0, action) => {
   switch(action.type) {
     case constants.SET_BALANCE:
       return action.balance;
+    case constants.DEPOSIT:
+      return state + action.deposit;
+    case constants.WITHDRAW:
+      return state - action.withdraw;
     default: 
       return state;
-  }
-}
+  };
+};
--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -7,5 +7,23 @@ describe('balanceReducer', () => {
     const test_action = { type: constants.SET_BALANCE, balance };
 
     expect(balanceReducer(undefined, test_action)).toEqual(balance);
-  })
-})
+  });
+
+  it('deposits into balance', () => {
+    const deposit = 10;
+    const initalState = 5;
+    const newTotal = deposit + initalState;
+    const test_action = { type: constants.DEPOSIT, deposit };
+
+    expect(balanceReducer(initalState, test_action)).toEqual(newTotal)
+  });
+
+  it('withdraws from the balance', () => {
+    const withdraw = 10;
+    const initalState = 20;
+    const newTotal = initalState - withdraw;
+    const test_action = { type: constants.WITHDRAW, withdraw };
+
+    expect(balanceReducer(initalState, test_action)).toEqual(newTotal);
+  });
+});
