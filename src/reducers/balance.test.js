18015b019a3fae593740f56158524778b05c95ee
chris-malloy <chrismalloymusic@gmail.com>
2018-03-12 12:45:24 -0400

    cookie feature added and passing

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -1,12 +1,22 @@
 import balanceReducer from './balance';
+import balanceReducer2 from './balance';
 import * as constants from '../actions/constants';
 
 describe('balanceReducer', () => {
-  it('sets a balance', () => {
+  describe('when initalizing', () => {
     const balance = 10;
-    const test_action = { type: constants.SET_BALANCE, balance };
+    
+    it('sets a balance', () => {  
+      const test_action = { type: constants.SET_BALANCE, balance };
 
-    expect(balanceReducer(undefined, test_action)).toEqual(balance);
+      expect(balanceReducer(undefined, test_action)).toEqual(balance);
+    });
+
+    describe('then re-initializing', () => {
+      it('reads the balance from cookies', () => {
+        expect(balanceReducer2(undefined, {})).toEqual(balance);
+      });
+    });
   });
 
   it('deposits into balance', () => {
