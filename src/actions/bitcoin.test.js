7bc3c62277b76e9628551fbe00271154146fc2d2
chris-malloy <chrismalloymusic@gmail.com>
2018-03-19 22:38:28 -0400

    bitcoin reducer test passing

--- src/actions/bitcoin.test.js
+++ src/actions/bitcoin.test.js
@@ -1,4 +1,3 @@
-// https://api.coindesk.com/v1/bpi/currentprice.json
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import fetchMock from 'fetch-mock';
@@ -6,6 +5,7 @@ import { FETCH_BITCOIN } from './constants';
 import { fetchBitcoin } from './bitcoin';
 
 const createMockStore = configureMockStore([thunk]);
+
 const store = createMockStore({ bitcoin: {} });
 
 const mockResponse = { body: { bpi: 'bitcoin price index' }};
